f87faf105b46ce253240eca833b2af848a3a317b
Trung DT <thanhtrung.it25@gmail.com>
2024-07-07 18:11:49 +0700

    ci: fix auth fixture

--- tests/fixtures/AuthFixture.js
+++ tests/fixtures/AuthFixture.js
@@ -5,21 +5,14 @@ export class AuthFixture {
 
   async signUpAndLogIn() {
     const testUser = 'test' + Date.now()
-
     await this.page.goto('/signup')
-    await this.page.getByLabel('Username:').click()
     await this.page.getByLabel('Username:').fill(testUser)
-    await this.page.getByLabel('Password:').click()
-    await this.page.getByLabel('Password:').fill('test')
+    await this.page.getByLabel('Password:').fill('password')
     await this.page.getByRole('button', { name: 'Sign Up' }).click()
-
     await this.page.waitForURL('**/login')
-    await this.page.getByLabel('Username:').click()
     await this.page.getByLabel('Username:').fill(testUser)
-    await this.page.getByLabel('Password:').click()
-    await this.page.getByLabel('Password:').fill('test')
+    await this.page.getByLabel('Password:').fill('password')
     await this.page.getByRole('button', { name: 'Log In' }).click()
-
     await this.page.waitForURL('**/')
     return testUser
   }
